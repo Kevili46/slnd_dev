291b4a843fc6a335830bb38c849f89d61a4ce0d7
Kevili46 <102224599+Kevili46@users.noreply.github.com>
2025-03-20 11:54:21 +0100

    about and basics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { WorkComponent } from './components/work/work.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AboutComponent, title: 'About | slnd' },
+    { path: 'work', component: WorkComponent, title: 'Work | slnd' },
+    { path: 'contact', component: ContactComponent, title: 'Contact | slnd' }
+];
